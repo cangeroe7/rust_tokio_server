8a58413bad38accd1af7b7e9e7a3b5c052348171
cangeroe7 <cangeroe7@gmail.com>
2024-02-05 11:28:04 -0600

    Added the rust server and static files

---
+++ rust_server/static/script.js
@@ -0,0 +1,218 @@
+const titleTextHold = {
+    "home": {
+        title: "History of Measurement",
+        text: "The earliest recorded systems of weights and measures originate in the 3rd or 4th millennium BC. Even the very earliest civilizations needed measurement for purposes of agriculture, construction and trade. Early standard units might only have applied to a single community or small region, with every area developing its own standards for lengths, areas, volumes and masses. Often such systems were closely tied to one field of use, so that volume measures used, for example, for dry grains were unrelated to those for liquids, with neither bearing any particular relationship to units of length used for measuring cloth or land. With development of manufacturing technologies, and the growing importance of trade between communities and ultimately across the Earth, standardized weights and measures became critical. Starting in the 18th century, modernized, simplified and uniform systems of weights and measures were developed, with the fundamental units defined by ever more precise methods in the science of metrology. The discovery and application of electricity was one factor motivating the development of standardized internationally applicable units.",
+        calcName: ""
+    },
+    "length": {
+        title: "History of Length Units",
+        text: "The way base units of length have been determined has changed greatly over time. Long ago, the base for reference was the human body. For example, the cubit was a unit that indicated the length from the elbow to the fingertips. This unit was used in ancient cultures in Mesopotamia, Egypt, and Rome. The length varied from region to region, ranging from 450 to 500 mm. Studies have proven that the pyramids of Egypt, known for their accurate construction, were built using two types of cubits: a long one and a short one. It is said that the standard measure of length in these eras was the body of the country's ruler or some other powerful individual. Even today, units of length based on the human body are used in countries such as the United States, such as the yard, foot, and inch. </br></br>Length units based on the human body were used for thousands of years. This continued until a major change took place around 200 years ago. As the Age of Discovery came to an end and industry grew primarily in Western Europe, it became necessary to unify units of length on a global scale. In the 17th century, discussions were held in Europe regarding the unification of units. After a century of discussions, France proposed the unit of the meter (meaning \"to measure\" in Greek) in 1791. The reference at this time was the distance of the meridian from the north pole to the equator. One meter was set as 1/10,000,000 of this distance. Later, a prototype meter using an alloy of platinum and iridium, which is highly resistant to oxidation and abrasion, was created in France at the end of the 19th century as a result of the need to unify dimension references on a global scale.",
+        calcName: "Length Conversion Calculator"
+    },
+    "weight": {
+        title: "Historty of Weight Units",
+        text: "Weight units have a long and complex history that spans thousands of years and encompasses many cultures and civilizations. Some of the earliest known weight units were based on the weight of various objects, such as seeds or stones. For example, the ancient Egyptians used a unit called the deben, which was based on the weight of a specific number of copper or gold beads.</br></br>In ancient Greece, a system of weight units was developed based on the talent, a unit of weight that was equivalent to the weight of water in a specific container. This system was later adopted by the Romans, who also developed their own weight units, such as the libra (pound) and the uncia (ounce).</br></br>During the Middle Ages, weight units varied widely across Europe, with each town and region having its own system. However, by the late medieval period, a more standardized system of weight units had emerged, based on the pound, ounce, and grain.</br></br>In the 17th century, the invention of the balance scale and the development of accurate measuring instruments led to the development of more precise weight units. The avoirdupois system, which is still in use today, was developed in the 17th century, and it defined the pound as exactly 16 ounces.</br></br>In the late 19th century, the International System of Units (SI) was established, which defined the kilogram as the base unit of mass. This system, which is based on the metric system, is now used in most of the world and has largely replaced the various traditional weight units.</br></br>The weight units have been further refined over time, with the current standard of weight being the International System of Units (SI) which defines the kilogram as the base unit of mass. The SI is now used globally and has replaced most of the traditional weight units. However, some traditional weight units are still in use in specific regions or industries, such as the pound and ounce in the United States.",
+        calcName: "Weight Conversion Calculator"
+    },
+    "temperature": {
+        title: "History of Temperature Units",
+        text: "Heat is a measure of the energy in a body or material — the more energy, the hotter it is. But unlike physical properties of mass and length, it's been difficult to measure. Most methods have been indirect, observing the effect that heat has on something and deducing temperature from this.</br></br>Creating a scale of measurement has been a challenge, too. In 1664, Robert Hooke proposed the freezing point of water be used as a zero point, with temperatures being measured from this. Around the same time, Ole Roemer saw the need for two fixed points, allowing interpolation between them. The points he chose were Hooke's freezing point and also the boiling point of water. This, of course, leaves open the question of how hot or cold things can get.</br></br>That was answered by Gay-Lussac and other scientists working on the gas laws. During the 19th century, while investigating the effect of temperature on gas at a constant pressure, they observed that volume rises by the fraction of 1/267 per degree Celsius, (later revised to 1/273.15). This led to the concept of absolute zero at minus 273.15°C.",
+        calcName: "Temperature Conversion Calculator"
+    },
+    "area": {
+        title: "History of Area Units",
+        text: "The concept of measuring area dates back to ancient civilizations such as the Egyptians and the Mesopotamians. They used units such as the aroura, which was roughly equivalent to 10,000 square meters, and the ikkar, which was around 624 square meters. The ancient Greeks also had their own units of area measurement, such as the stremma, which was about 1,000 square meters.</br></br>During the Middle Ages, the concept of area measurement was primarily used for land measurement for the purposes of taxation and land ownership. The unit of measurement varied depending on the region, with units such as the hide in England and the ploughland in Europe being commonly used.</br></br>In the 17th and 18th centuries, with the rise of the scientific revolution and the Age of Enlightenment, there was a push for more standardized units of measurement. The introduction of the metric system in the late 18th century brought about the use of the square meter as the standard unit of area measurement.</br></br>The International System of Units (SI) was established in 1960, which defined the square meter as the standard unit of area measurement. However, other units such as the square kilometer and the hectare are also commonly used, particularly in land measurement and agriculture.</br></br>In recent years, with the advancement of technology, there has been a greater need for precise measurements in fields such as engineering and construction. As a result, there has been a rise in the use of smaller units of area measurement, such as the square millimeter and the square micrometer.</br></br>Overall, the history of area units is a reflection of human civilization's evolution of how to measure land, from ancient civilizations using rough approximations, to a more standardized approach in recent centuries.",
+        calcName: "Area Conversion Calculator"
+    },
+    "volume": {
+        title: "History of Volume Units",
+        text: "In the Middle Ages, many units for measuring volume were made, such as the sester, amber, coomb, and seam. The sheer quantity of such units motivated British kings to standardize them, culminated in the Assize of Bread and Ale statute in 1258 by Henry III of England. The statute standardized weight, length and volume as well as introduced the peny, ounce, pound, gallon and bushel. In 1618, the London Pharmacopoeia (medicine compound catalog) adopted the Roman gallon or congius as a basic unit of volume and gave a conversion table to the apothecaries' units of weight. Around this time, volume measurements are becoming more precise and the uncertainty is narrowed to between 1-5 mL (0.03-0.2 US fl oz; 0.04-0.2 imp fl oz).</br></br>Around the early 17th century, Bonaventura Cavalieri applied the philosophy of modern integral calculus to calculate the volume of any object. He devised the Cavalieri's principle, which said that using thinner and thinner slices of the shape would make the resulting volume more and more accurate. This idea would then be later expanded by Pierre de Fermat, John Wallis, Isaac Barrow, James Gregory, Isaac Newton, Gottfried Wilhelm Leibniz and Maria Gaetana Agnesi in the 17th and 18th centuries to form the modern integral calculus that is still being used in the 21st century.",
+        calcName: "Volume Conversion Calculator"
+    }
+};
+
+const calculatorOptions = {
+    "length": [
+        ["Meter", 1],
+        ["Kilometer", 0.001],
+        ["Centimeter", 100],
+        ["Millimeter", 1000],
+        ["Micrometer", 1000000],
+        ["Nanometer", 1000000000],
+        ["Mile", 0.0006213689],
+        ["Yard", 1.0936132983],
+        ["Foot", 3.280839895],
+        ["Inch", 39.37997874],
+        ["Light Year", 1.057008707*10**-16]
+    ],
+    "weight": [
+        ["Kilogram", 1],
+        ["Gram", 1000],
+        ["Milligram", 1000000],
+        ["Pound", 2.2046244202],
+        ["Ounce", 35.273990723],
+        ["Carrat", 5000],
+        ["Metric Ton", 0.001],
+        ["Long Ton", 0.0009842073],
+        ["Short Ton", 0.0011023122],
+        ["Atomic Mass Unit", 6.022136652*10**26]
+    ],
+    "temperature": [
+        ["Celsius", 0],
+        ["Kelvin", 0],
+        ["Fahrenheit", 0]
+    ],
+    "area": [
+        ["Square Meter", 1],
+        ["Square Kilometer", 0.000001],
+        ["Square Centimeter", 10000],
+        ["Square Millimeter", 1000000],
+        ["Square Micrometer", 1000000000000],
+        ["Hectare", 0.0001],
+        ["Square Mile", 3.861018768*10**-7],
+        ["Square Yard", 1.1959900463],
+        ["Square Yard", 10.763910417],
+        ["Square Inch", 1550.0031],
+        ["Acre", 0.0002471054],
+    ],
+    "volume": [
+        ["Cubic Meter", ],
+        ["Cubic Kilometer", ],
+        ["Cubic Centimeter", ],
+        ["Cubic Millimeter", ],
+        ["Liter", ],
+        ["Milliliter", ],
+        ["US Gallon", ],
+        ["US Quart", ],
+        ["US Pint", ],
+        ["US Cup", ],
+        ["US Fluid Ounce", ],
+        ["US Table Spoon", ],
+        ["US Tea Spoon", ],
+        ["Imperial Gallon", ],
+        ["Imperial Pint", ],
+        ["Imperial Cup", ],
+        ["Imperial Fluid Ounce", ],
+        ["Imperial Table Spoon", ],
+        ["Imperial Tea Spoon", ],
+        ["Cubic Mile", ],
+        ["Cubic Yard", ],
+        ["Cubic Foot", ],
+        ["Cubic Inch", ]
+    ]
+};
+
+let currentButton = document.getElementById("home");
+let startUnit, finalUnit;
+
+const onStart = () => {
+    changeText("home");
+    changeCurrentButton("home");
+    addEventsToNavButtons();
+}
+
+// changes the title of the calculator, and the little text's title and content below the calculator.
+const changeText = (measurementId) => {
+    const historyTitle = document.getElementById("history-title");
+    const historyText = document.getElementById("history-text");  
+    const calculatorName = document.getElementById("calculator-name");
+    historyTitle.innerHTML = titleTextHold[measurementId].title;
+    historyText.innerHTML = titleTextHold[measurementId].text;
+    calculatorName.innerHTML = titleTextHold[measurementId].calcName;
+}
+
+const changeCurrentButton = (buttonId) => {
+    currentButton.style.backgroundColor = "";
+    currentButton = document.getElementById(buttonId);
+    currentButton.style.backgroundColor = "#04AA6D";
+
+}
+
+// creates the calculator for the right units 
+const createCalculator = (calculatorId) => {
+
+}
+
+// handles the change from calculator to calculator and going back to home
+const handleNavChange = ({target}) => {
+    changeText(target.id);
+    changeCurrentButton(target.id);
+    if (target.id === "home") {
+        document.getElementById("calculation-hold").style.display = "none";
+        return
+    } else {
+        document.getElementById("calculation-hold").style.display = "grid";
+    }
+    createOptions(target.id);
+}
+
+// gives every nav button an handleNavChange click event
+const addEventsToNavButtons = () => {
+    const buttons = document.getElementsByClassName("measure-button");
+    for (let i=0; i < buttons.length; i++) {
+        let button = buttons.item(i)
+        button.addEventListener('click', handleNavChange);
+    }
+};
+
+// gives the input and output columns the options of the specific calculator
+const createOptions = (calculatorId) => {
+    const inputOptions = document.getElementById("input-unit");
+    const outputOptions = document.getElementById("output-unit");
+    removeOptions(inputOptions, outputOptions);
+    addOptions(calculatorId, inputOptions, outputOptions);
+
+}
+
+// removes the current options from the calculator options
+const removeOptions = (inputOptions, outputOptions) => {
+    while (inputOptions.options.length > 0) {
+        inputOptions.remove(0);
+        outputOptions.remove(0);
+    }
+
+}
+
+// add the new calculator options that correspond with the selected nav button
+const addOptions = (calculatorId, inputOptions, outputOptions) => {
+    calculatorOptions[calculatorId].forEach(option => {
+        let newOption1 = new Option(option[0], option[0]);
+        let newOption2 = new Option(option[0], option[0]);
+        outputOptions.add(newOption1);
+        inputOptions.add(newOption2); 
+    });
+}
+
+
+
+
+
+// const updateResult = () => {
+//     if
+// }
+
+const handleUnitChange = () => {
+    console.log("see this tom")
+}
+
+const handleValueChange = (value) => {
+    console.log("handle value change for this one")
+}
+
+const fromSelectionForm = document.getElementById('input-unit');
+const toSelectionForm = document.getElementById('output-unit');
+
+fromSelectionForm.addEventListener('change', handleUnitChange);
+toSelectionForm.addEventListener('change', handleUnitChange);
+
+const valueInput = document.getElementById('from-val');
+valueInput.addEventListener('keyup', handleValueChange);
+
+// const addEventsToNavButtons = () => {
+//     const buttons = document.getElementsByClassName("measure-button");
+//     for (let i=0; i < buttons.length; i++) {
+//         let button = buttons.item(i)
+//         button.addEventListener('click', handleNavChange);
+//     }
+
+onStart(); 
